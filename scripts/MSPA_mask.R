09b3ecd6dc2d3a856cd619ff56b860782320b7be
yfinegold <yelena.finegold@fao.org>
2019-10-29 23:39:40 +0000

    update

--- scripts/MSPA_mask.R
+++ scripts/MSPA_mask.R
@@ -1,22 +1,22 @@
 ## prepare forest mask for MSPA analysis
 # land cover map
-source('~/uga_activity_data/scripts/get_parameters.R')
+source('~/ghana_bfast_landtrendR/scripts/s0_parameters.R')
 
-lc <- paste0(lc_dir,'Ug2017_CW_gEdits4_co.tif')
+lc <- paste0(lc_dir,'forest_mask_2000_2015.tif')
 
 # output mask for MSPA analysis
-mspa_mask <- paste0(mspa_dir,'THF_mask_MSPA.tif')
+mspa_mask <- paste0(mspa_dir,'forest_mask_MSPA.tif')
 #################### reclassify LC map into THF mask
 system(sprintf("gdal_calc.py -A %s --type=Byte --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                lc,
                mspa_mask,
-               paste0("((A==3)+(A==4))*2+((A<3)+(A>4))*1")
+               paste0("((A==1))*2+((A<1))*1")
 ))
-#################### reproject THF mask to latlong WGS84
+#################### reproject forest mask to latlong WGS84
 system(sprintf("gdalwarp -t_srs \"%s\" -overwrite -ot Byte -co COMPRESS=LZW %s %s",
                "EPSG:4326",
                mspa_mask,
                paste0(mspa_dir,"proj.tif")
 ))
-gdalinfo(mspa_mask,mm=T)
+# gdalinfo(mspa_mask,mm=T)
 plot(raster(mspa_mask))
